3ac1760b35e7acdd28be49bed9d038e492669d34
Cheryl Wu <grungerabbit@gmail.com>
2016-01-15 22:25:22 -0500

    simple plot mood data

--- date-timeline/js/timeline.js
+++ date-timeline/js/timeline.js
@@ -44,6 +44,16 @@ $(document).ready(function() {
                 drawSeg(tickStart, tickEnd);
             }
 
+            /*var outputObject = []*/
+
+            function drawMoodScale(mood) {
+                return 65 + (20 * mood);
+            }
+
+            for (var a = 1; a <= 5; a++) {
+                paper.circle(drawMoodScale(a), verticalOffset, 5);
+            }
+
             function makeTimeline() {
                 var year = 25;
                 var month = 10;
@@ -63,18 +73,36 @@ $(document).ready(function() {
                     drawTick(tick, tickOffset);
                     paper.text(200, tickOffset, currentMonth.format('MM YYYY'));
 
+                   
+                    var key;
+                    var monthOffset = currentMonth.month() + 1;
+
+                    if (monthOffset < 10) {
+                        key = '0' + monthOffset + '-' + currentMonth.year();
+                    } else {
+                        key = '' + monthOffset + '-' + currentMonth.year();
+                    }
+
+                    console.log()
+
+                    var currentMood = chronology.moodData[key];
+
+                    paper.circle(drawMoodScale(currentMood), tickOffset, 3)
+
+                    //outputObject.push("'" + key + "'" + ": { 3 }");*/
 
                     tickOffset += monthIncrement;
                 }
-
             }
 
             makeTimeline();
 
+            /*console.log(outputObject)*/
+
             var currentModality = 40;
 
             for (var legend in chronology.settings.modalities) {
-                console.log(legend);
+                //console.log(legend);
                 var modColor = chronology.settings.modalities[legend];
 
                 paper.text(50, currentModality, legend).attr({fill: modColor, 'font-weight': 'bold'})
@@ -84,10 +112,12 @@ $(document).ready(function() {
 
             var dayIncrement = lineHeight / daysSinceStart;
 
-            for (var j = 0; j < chronology.data.length; j++) {
-                console.log(chronology.data[j]);
 
-                var currentPerson = chronology.data[j];
+            // encounter loops
+            for (var j = 0; j < chronology.encounterData.length; j++) {
+                //console.log(chronology.encounterData[j]);
+
+                var currentPerson = chronology.encounterData[j];
                 var personStart = moment(currentPerson.appxStartDate);
                 var personEnd = currentPerson.appxEndDate === null ? nowMoment : moment(currentPerson.appxEndDate);
                 var daysSincePerson = personStart.diff(startMoment, 'days');
@@ -120,7 +150,83 @@ $(document).ready(function() {
         'getPoint': function(x, y) {
             return x + ',' + y + ',';   
         },
-        'data': [
+        'lifeData': [
+            {
+                'appxStartDate': '2010-09-01',
+                'appxEndDate': '2011-06-01',
+                'event': ''
+            }
+        ],
+        'moodData': {
+            '07-2010': 4,
+            '08-2010': 4,
+            '09-2010': 2,
+            '10-2010': 3,
+            '11-2010': 2,
+            '12-2010': 3,
+            '01-2011': 4,
+            '02-2011': 3,
+            '03-2011': 2,
+            '04-2011': 3,
+            '05-2011': 4,
+            '06-2011': 4,
+            '07-2011': 3,
+            '08-2011': 4,
+            '09-2011': 4,
+            '10-2011': 3.5,
+            '11-2011': 4,
+            '12-2011': 4,
+            '01-2012': 4,
+            '02-2012': 3,
+            '03-2012': 3,
+            '04-2012': 2,
+            '05-2012': 4,
+            '06-2012': 5,
+            '07-2012': 4.5,
+            '08-2012': 5,
+            '09-2012': 3.5,
+            '10-2012': 4,
+            '11-2012': 4,
+            '12-2012': 5,
+            '01-2013': 2,
+            '02-2013': 2,
+            '03-2013': 1,
+            '04-2013': 2,
+            '05-2013': 2,
+            '06-2013': 1,
+            '07-2013': 3,
+            '08-2013': 4,
+            '09-2013': 3,
+            '10-2013': 4,
+            '11-2013': 3,
+            '12-2013': 3,
+            '01-2014': 4,
+            '02-2014': 4,
+            '03-2014': 4,
+            '04-2014': 3,
+            '05-2014': 3,
+            '06-2014': 2,
+            '07-2014': 3,
+            '08-2014': 3,
+            '09-2014': 2,
+            '10-2014': 4,
+            '11-2014': 3,
+            '12-2014': 3,
+            '01-2015': 4,
+            '02-2015': 4,
+            '03-2015': 2,
+            '04-2015': 2,
+            '05-2015': 1,
+            '06-2015': 2,
+            '07-2015': 2.5,
+            '08-2015': 4,
+            '09-2015': 4,
+            '10-2015': 5,
+            '11-2015': 3,
+            '12-2015': 4,
+            '01-2016': 2
+        },
+        'encounterData': [
             {
                 'appxStartDate': '2010-07-04',
                 'appxEndDate': '2010-07-20',
